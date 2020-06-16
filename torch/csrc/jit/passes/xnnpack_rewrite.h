2a18a7b4c04a805f561337bc4b4fda1c9a3c0116
Xingying Cheng <xcheng16@fb.com>
2020-06-15 23:11:19 -0700

    Add whitelist optimization dict as second arg to optimizeForMobile method. Summary: Instead of running all the optimization pass in optimizeForMobile method, introducing a whitelist optimizer dictionary as second param in the method, when it is not passed during calling, the method will run all the optimization passes, otherwise the method will read the dict and only run the pass with value of True.
    Test Plan:
    python test/test_mobile_optimizer.py
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 80006b7e46625176d382a66f9a1e23a0f414afe4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/37462

--- torch/csrc/jit/passes/xnnpack_rewrite.h
+++ torch/csrc/jit/passes/xnnpack_rewrite.h
@@ -5,11 +5,20 @@
 
 namespace torch {
 namespace jit {
+
+enum class MobileOptimizerType {
+  CONV_BN_FUSION,
+  FOLD_CONV_BATCH_NORM,
+  INSERT_FOLD_PREPACK_OPS,
+  REMOVE_DROPOUT
+};
+
 TORCH_API void insertPrePackedOps(std::shared_ptr<Graph>& graph);
 TORCH_API void insertPrePackedOps(script::Module& module);
 TORCH_API void fusePrePackedLinearConvWithClamp(script::Module& module);
 TORCH_API void FoldPrePackingOps(script::Module& module);
-TORCH_API c10::optional<script::Module> optimizeForMobile(
-    const script::Module& module);
+TORCH_API script::Module optimizeForMobile(
+    const script::Module& module,
+    const std::unordered_set<MobileOptimizerType>& optimization_blacklist);
 } // namespace jit
 } // namespace torch
